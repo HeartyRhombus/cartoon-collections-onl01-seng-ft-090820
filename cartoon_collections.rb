d6a9c74390f17ab808c13f179a934aa5e128984d
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-10 22:56:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? {|word| word.length > 4}
-  calls.all? {|word| word.length > 4}
+   if calls.any? {|word| word.length > 4}
+  elsif calls.all? {|word| word.length > 4}
+  end
 end
 
 def find_the_cheese# code an argument here
