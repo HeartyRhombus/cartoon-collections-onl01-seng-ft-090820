76e6aaa5efda97a91231e0df38ef136c53a5974f
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-10 23:47:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,9 +26,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if cheese_types.any? do |cheese|
     (array.include? cheese)
+    return cheese
     end
-  return cheese
-  else
-    nil
   end
 end
