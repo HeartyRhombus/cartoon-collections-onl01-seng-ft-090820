87bd1cc15c94aa3a2e1ac9beb276ce97dc0db507
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-10 23:09:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,6 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-  array.include?(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?(cheese_types)
 end
